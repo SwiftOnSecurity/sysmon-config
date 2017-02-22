0f3a3f53c0615314ca51c67851f25f5717dceeb6
defcon <defconoii@gmail.com>
2017-02-21 20:18:59 -0500

    Download sysmon to ProgramData rather than temp location.

--- Install Sysmon.bat
+++ Install Sysmon.bat
@@ -1,9 +1,10 @@
 @echo off
-cd %temp%
+mkdir C:\ProgramData\sysmon
+cd C:\ProgramData\sysmon\
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','%temp%\sysmon.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\ProgramData\sysmon\sysmon.exe')"
 echo [+] Downloading Sysmon config...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ion-storm/sysmon-config/master/sysmonconfig-export.xml','%temp%\sysmonconfig-export.xml')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ion-storm/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
 sysmon.exe -accepteula -i sysmonconfig-export.xml
 echo [+] Sysmon Successfully Installed!
 timeout /t 10
