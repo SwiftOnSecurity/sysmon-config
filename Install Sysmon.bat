ae00a73fa555d0feedee1f98b7ba566368b70590
def ccon <defconoii@gmail.com>
2017-02-22 08:00:58 -0500

    Update Sysmon to 64bit

--- Install Sysmon.bat
+++ Install Sysmon.bat
@@ -2,10 +2,10 @@
 mkdir C:\ProgramData\sysmon
 cd C:\ProgramData\sysmon\
 echo [+] Downloading Sysmon...
-@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon.exe','C:\ProgramData\sysmon\sysmon.exe')"
+@powershell (new-object System.Net.WebClient).DownloadFile('https://live.sysinternals.com/Sysmon64.exe','C:\ProgramData\sysmon\sysmon64.exe')"
 echo [+] Downloading Sysmon config...
 @powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ion-storm/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-sysmon.exe -accepteula -i sysmonconfig-export.xml
+sysmon64.exe -accepteula -i sysmonconfig-export.xml
 echo [+] Sysmon Successfully Installed!
 timeout /t 10
 exit
