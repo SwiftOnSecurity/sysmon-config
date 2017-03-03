ad32c19a8d6b10d17dc1d8a16ac62b31dc911bac
def ccon <defconoii@gmail.com>
2017-03-02 21:42:32 -0500

    Add noprompt installer, you know what to do

---
+++ Graylog_Content_Pack/Install_Sidecar_noprompt.bat
@@ -0,0 +1,16 @@
+@echo off
+cd %temp%
+echo [+] Downloading Graylog Sidecar to: %temp%\Sidecar.exe...
+@powershell (new-object System.Net.WebClient).DownloadFile('https://github.com/Graylog2/collector-sidecar/releases/download/0.1.0-rc.1/collector_sidecar_installer_0.1.0-rc.1.exe','%temp%\Sidecar.exe')"
+start /wait Sidecar.exe /S -SERVERURL=https://YOURSERVERIPHERE:443/api -TAGS="windows"
+echo [+] Executing Script to edit content of sidecar configuration...
+@powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://goo.gl/mYNrH7')"
+cd "C:\Program Files\graylog\collector-sidecar\"
+echo [+] Installing Graylog Services...
+graylog-collector-sidecar.exe -service install
+graylog-collector-sidecar.exe -service start
+echo [+] Checking Services...
+@powershell get-service collector-sidecar
+echo [+] Graylog Sidecar Successfully Installed and Configured!
+timeout /t 10
+exit
