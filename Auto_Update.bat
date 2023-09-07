e60c40b72a6ff1e19e73d17df84a86dd4a77a7e0
NerbalOne <nerbalone@gmail.com>
2023-09-07 09:30:45 -0400

    Delete Auto_Update.bat

--- Auto_Update.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-cd C:\ProgramData\sysmon\
-@powershell (new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/ion-storm/sysmon-config/master/sysmonconfig-export.xml','C:\ProgramData\sysmon\sysmonconfig-export.xml')"
-sysmon64 -c sysmonconfig-export.xml
-exit
